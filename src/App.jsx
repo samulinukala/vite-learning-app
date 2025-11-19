a9a5c32479963cce9eaa1d61d91c48decf97746e
samulinukala <samulinukala740@gmail.com>
2025-11-19 16:40:16 +0200

    started proper aadasdasdasdaaaaaaaaaaaaadaspdappsdpaspdappsdpaspdspdpddpaassdppappsdpasppdassd

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,37 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+function test()
+{
+ alert("test"); 
 
-function App() {
+}
+let images=[];
+//add a function for getting images to array
+function getImg(){
+  
+  const context=require.context('./src/img',false,/\.(png|jpe?g|svg)$/);
+  images=context.keys().map(context);
+  
+  alert(images.length);
+}
+const AddImagesButton = () =>{
+  return (
+    <div>
+     <button onClick={test}> test </button>
+      <button onClick={getImg}>test img</button>
+      </div>c
+    )
+}
+const App = () =>{
   return (
     <div className="App">
       <h1>Hello World!</h1>
-      <img></img>
+      <AddImagesButton/>
+     
     </div>
   );
 }
 
 
-export default App
+export default App;
